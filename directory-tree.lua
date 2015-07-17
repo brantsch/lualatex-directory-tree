80eb64c91a9360f8b18b54ba89a81253c1ecf2f1
Peter Brantsch <peter.brantsch@kabelmail.de>
2015-07-18 01:07:38 +0200

    improve customizability

--- directory-tree.lua
+++ directory-tree.lua
@@ -2,22 +2,39 @@ tex = require "tex";
 
 local directoryTree = { }
 
+directoryTree.label = function (attr, path)
+  local label = "\\textit{WARNING: NO LABEL}";
+  if attr.mode == "directory" then
+    label = path .. '/';
+  else
+    label = path;
+  end
+  label = label:gsub("_","\_");
+  return label;
+end
+
+directoryTree.style = function (attr, path)
+  local style = "";
+  if attr.mode == "directory" then
+    style = "directory";
+  else
+    style = "file";
+  end
+  return style;
+end
+
 directoryTree.printNode = function (path,level)
   if not level then level = 0; end
   local attr, msg = lfs.attributes(path);
   if (not attr) and msg then
     print(msg);
   else
-    if attr.mode == "directory" then
-      label = path .. '/';
-    else
-      label = path;
-    end
-    label = label:gsub("_","\_");
+    local label = directoryTree.label(attr, path);
+    local style = directoryTree.style(attr, path);
     if level == 0 then
-      tex.print("\\node{" .. label .. "}");
+      tex.print("\\node[" .. style .. "] {" .. label .. "}");
     else
-      tex.print("child{ node{" .. label .. "}");
+      tex.print("child{ node[" .. style .. "] {" .. label .. "}");
     end
     if attr.mode == "directory" then
       for entry in lfs.dir(path) do
